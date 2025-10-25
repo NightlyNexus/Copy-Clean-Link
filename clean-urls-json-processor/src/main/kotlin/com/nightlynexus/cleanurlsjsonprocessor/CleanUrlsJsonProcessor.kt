c6b01f00640c667c3c1a96c507c4b5d7b63333c8
Eric Cochran <Eric@NightlyNexus.com>
2025-10-25 04:11:52 -0400

    Update clean_urls.json from Brave.

--- clean-urls-json-processor/src/main/kotlin/com/nightlynexus/cleanurlsjsonprocessor/CleanUrlsJsonProcessor.kt
+++ clean-urls-json-processor/src/main/kotlin/com/nightlynexus/cleanurlsjsonprocessor/CleanUrlsJsonProcessor.kt
@@ -18,7 +18,7 @@ import com.squareup.moshi.Types
 import okio.buffer
 import okio.source
 
-// https://github.com/brave/adblock-lists/blob/653cbb7f1d96092307f630d3fc9e2f64985c5570/brave-lists/clean-urls.json
+// https://github.com/brave/adblock-lists/blob/dd3b1d35f871bd2ad59d28fe1c3ee822dfd32f22/brave-lists/clean-urls.json
 internal class CleanUrlsJsonProcessor(
   private val codeGenerator: CodeGenerator
 ) : SymbolProcessor {
